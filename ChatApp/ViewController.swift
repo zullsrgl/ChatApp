99678a1cd3bad6a024e89c5de58a726434f4c3cb
zulalsarioglu <zulal.sariogluu@gmail.com>
2025-09-11 00:52:09 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Zülal Sarıoğlu on 11.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
